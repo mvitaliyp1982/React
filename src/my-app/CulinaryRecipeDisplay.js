ca8f9b6995f3d29c48dc57a540635f61ed28e15d
mvitaliyp1982 <https://github.com/mvitaliyp1982/Ang.git>
2023-02-05 13:54:15 +0200

    react)

---
+++ src/my-app/CulinaryRecipeDisplay.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class CulinaryRecipeDisplay extends Component {
+    render() {
+      return (
+        <>
+        <hr></hr>
+        <h1>Displaying some CulinaryRecipe!</h1>
+          <table>
+            <tbody>
+              <tr><td>ggg</td><td>jjj</td></tr>
+              <tr><td>ggg</td><td>jjj</td></tr>
+              <tr><td>ggg</td><td>jjj</td></tr>
+            </tbody>
+          </table>
+        </>
+      );
+    }
+  }
+
+export default CulinaryRecipeDisplay;
---
+++ src/my-app/MusicGroupDisplay.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class MusicGroupDisplay extends Component {
+    render() {
+      return (
+        <>
+        <hr></hr>
+        <h1>Displaying some MusicGroup!</h1>
+          <table>
+            <tbody>
+              <tr><td>ggg</td><td>jjj</td></tr>
+              <tr><td>ggg</td><td>jjj</td></tr>
+              <tr><td>ggg</td><td>jjj</td></tr>
+            </tbody>
+          </table>
+        </>
+      );
+    }
+  }
+
+export default MusicGroupDisplay;
